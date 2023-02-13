1a4655c886b5f1d579ba8a1625d04346273d35a8
German Zabludin <g.zabludin@gmail.com>
2023-02-13 12:41:42 +0300

    --add hw files

---
+++ hw2/otus.health/Controllers/HealthCheckController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace otus.health.Controllers
+{
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/health")]
+        public ActionResult Health()
+        {
+            var result = new Result("OK");
+            return Ok(result);
+        }
+    }
+}
---
+++ hw2/otus.health/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace otus.health
+{
+    public class Result
+    {
+        public string status { get; set; }
+        public Result(string _result)
+        {
+            this.status = _result;
+        }
+    }
+}
